aac00e826463118ef92a9d064f81c79d55db7542
Matt Keeble <matt.keeble@gmail.com>
2016-09-05 19:41:45 +0100

    running and tests passing with no trace of typescript

--- test/BaseGenerator.spec.js
+++ test/BaseGenerator.spec.js
@@ -1,46 +1,42 @@
 'use strict';
+const BaseGenerator = require('../lib/BaseGenerator');
 require('chai').should();
-const BaseGenerator = require('../lib/BaseGenerator').BaseGenerator;
 
 describe('BaseGenerator class', () => {
 
-  it('should set default config on construction', () => {
-    let sut = new BaseGenerator();
-    let expected = {
-      numCircles: 1,
-      minMaxRad: 200,
-      maxMaxRad: 200,
-      minRadFactor: 0.2,
-      iterations: 8,
-      bgColor: '#ffffff',
-      urlColor: '#000000',
-      drawsPerFrame: 6
-    }
+    it('should set default config on construction', () => {
+        let sut = new BaseGenerator();
+        let expected = {
+            numCircles: 1,
+            minMaxRad: 200,
+            maxMaxRad: 200,
+            minRadFactor: 0.2,
+            iterations: 8,
+            bgColor: '#ffffff',
+            urlColor: '#000000',
+            drawsPerFrame: 6
+        }
+        sut.getConfig().should.deep.equal(expected);
+    });
 
-    sut.getConfig().should.deep.equal(expected);
-  });
+    it('should override config values passed to constructor', () => {
+        let newConfig = {
+            numCircles: 5,
+            minMaxRad: 1000
+        };
+        let expected = {
+            numCircles: 5,
+            minMaxRad: 1000,
+            maxMaxRad: 200,
+            minRadFactor: 0.2,
+            iterations: 8,
+            bgColor: '#ffffff',
+            urlColor: '#000000',
+            drawsPerFrame: 6
+        };
+        let sut = new BaseGenerator(newConfig);
+        sut.getConfig().should.deep.equal(expected);
+    });
 
-  it('should override config values passed to constructor', () => {
-    let newConfig = {
-      numCircles: 5,
-      minMaxRad: 1000
-    };
-    let expected = {
-      numCircles: 5,
-      minMaxRad: 1000,
-      maxMaxRad: 200,
-      minRadFactor: 0.2,
-      iterations: 8,
-      bgColor: '#ffffff',
-      urlColor: '#000000',
-      drawsPerFrame: 6
-    };
-    let sut = new BaseGenerator(newConfig);
-    sut.getConfig().should.deep.equal(expected);
-  });
-
-  it('should create a canvas object', () => {
-    let sut = new BaseGenerator();
-    sut.createCanvas().should.be.null;
-  });
+    it('should create a canvas object');
 });
