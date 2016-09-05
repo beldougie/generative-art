c9e5da1984e90625ccd0efbb7405486987beef6a
Matt Keeble <matt.keeble@gmail.com>
2016-09-05 22:41:52 +0100

    Feature/#1 initial class content (#11)
    * updating unit tests
    * introduce node-canvas
    * adding jshint ocnfig
    * updated the BaseGenerator with a lot of the basic code
      (modified from the SolidGrey example

--- test/BaseGenerator.spec.js
+++ test/BaseGenerator.spec.js
@@ -1,5 +1,6 @@
 'use strict';
 const BaseGenerator = require('../lib/BaseGenerator');
+const expect = require('chai').expect;
 require('chai').should();
 
 describe('BaseGenerator class', () => {
@@ -12,9 +13,12 @@ describe('BaseGenerator class', () => {
             maxMaxRad: 200,
             minRadFactor: 0.2,
             iterations: 8,
+            lineWidth: 3,
             bgColor: '#ffffff',
             urlColor: '#000000',
-            drawsPerFrame: 6
+            drawsPerFrame: 6,
+            canvasWidth: 200,
+            canvasHeight: 200
         }
         sut.getConfig().should.deep.equal(expected);
     });
@@ -30,13 +34,19 @@ describe('BaseGenerator class', () => {
             maxMaxRad: 200,
             minRadFactor: 0.2,
             iterations: 8,
+            lineWidth: 3,
             bgColor: '#ffffff',
             urlColor: '#000000',
-            drawsPerFrame: 6
+            drawsPerFrame: 6,
+            canvasWidth: 200,
+            canvasHeight: 200
         };
         let sut = new BaseGenerator(newConfig);
         sut.getConfig().should.deep.equal(expected);
     });
 
-    it('should create a canvas object');
+    it('should create a canvas object', () => {
+        let sut = new BaseGenerator();
+        expect(sut.createCanvas()).to.be.ok;
+    });
 });
