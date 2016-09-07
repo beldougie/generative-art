b4e3ea3104f5245f9cf96b291cfc91dd893b96d0
Matt Keeble <matt.keeble@gmail.com>
2016-09-07 18:18:10 +0100

    Refactored into the 'Solid' class

--- test/SolidGenerator.spec.js
+++ test/SolidGenerator.spec.js
@@ -1,24 +1,25 @@
 'use strict';
-const BaseGenerator = require('../lib/BaseGenerator');
+const SolidGenerator = require('../lib/SolidGenerator');
 const expect = require('chai').expect;
 require('chai').should();
 
 describe('BaseGenerator class', () => {
 
     it('should set default config on construction', () => {
-        let sut = new BaseGenerator();
+        let sut = new SolidGenerator();
         let expected = {
             numCircles: 1,
             minMaxRad: 200,
             maxMaxRad: 200,
             minRadFactor: 0.2,
+            lineWidth: 2,
             bgColor: '#ffffff',
             urlColor: '#000000',
             drawsPerFrame: 6,
             canvasWidth: 200,
             canvasHeight: 200
-        }
-        sut.getConfig().should.deep.equal(expected);
+        };
+        sut.config.should.deep.equal(expected);
     });
 
     it('should override config values passed to constructor', () => {
@@ -31,18 +32,19 @@ describe('BaseGenerator class', () => {
             minMaxRad: 1000,
             maxMaxRad: 200,
             minRadFactor: 0.2,
+            lineWidth: 2,
             bgColor: '#ffffff',
             urlColor: '#000000',
             drawsPerFrame: 6,
             canvasWidth: 200,
             canvasHeight: 200
         };
-        let sut = new BaseGenerator(newConfig);
-        sut.getConfig().should.deep.equal(expected);
+        let sut = new SolidGenerator(newConfig);
+        sut.config.should.deep.equal(expected);
     });
 
     it('should create a canvas object', () => {
-        let sut = new BaseGenerator();
+        let sut = new SolidGenerator();
         expect(sut.createCanvas()).to.be.ok;
     });
 });
