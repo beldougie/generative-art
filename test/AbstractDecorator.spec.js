d11aac1e00168d28f2a87bea1587d1c50638b076
Matt Keeble <matt.keeble@gmail.com>
2016-09-08 20:14:18 +0100

    reworked unit tests

--- test/AbstractDecorator.spec.js
+++ test/AbstractDecorator.spec.js
@@ -7,7 +7,18 @@ const TEST_CONFIG = require('../lib/generation-constants').SOLID_CONFIG;
 chai.should();
 describe('AbstractDecorator', () => {
 
-  it('should be easy to access the properties', () => {
+  it('construction without parameters should throw an error', () => {
+    let createEmpty = () => new AbstractDecorator();
+    let createNoCfg = () => new AbstractDecorator({});
+    let createNoCtx = () => new AbstractDecorator(undefined, {});
+
+
+    expect(createEmpty).to.throw(Error, 'ctx and config are required');
+    expect(createNoCfg).to.throw(Error, 'ctx and config are required');
+    expect(createNoCtx).to.throw(Error, 'ctx and config are required');
+  });
+
+  it('should allow configuration to be overriden', () => {
     let sut = new AbstractDecorator({}, TEST_CONFIG);
     expect(sut.numCircles).to.equal(TEST_CONFIG.numCircles);
     expect(sut.minMaxRad).to.equal(TEST_CONFIG.minMaxRad);
@@ -19,6 +30,31 @@ describe('AbstractDecorator', () => {
     expect(sut.drawsPerFrame).to.equal(TEST_CONFIG.drawsPerFrame);
     expect(sut.displayWidth).to.equal(TEST_CONFIG.canvasWidth);
     expect(sut.displayHeight).to.equal(TEST_CONFIG.canvasHeight);
-    expect(sut.iterations).to.equal(8);
+    expect(sut.iterations).to.equal(TEST_CONFIG.iterations);
+  });
+
+  it('should throw an exception when begin() is called', () => {
+    let sut = new AbstractDecorator({}, TEST_CONFIG);
+    expect(sut.begin).to.throw(Error, 'Error! AbstractDecorator#begin must be overridden');
+  });
+
+  it('should throw an exception when onTimer() is called', () => {
+    let sut = new AbstractDecorator({}, TEST_CONFIG);
+    expect(sut.onTimer).to.throw(Error, 'Error! AbstractDecorator#onTimer must be overridden');
+  });
+
+  it('should throw an exception when setCircles() is called', () => {
+    let sut = new AbstractDecorator({}, TEST_CONFIG);
+    expect(sut.setCircles).to.throw(Error, 'Error! AbstractDecorator#setCircles must be overridden');
+  });
+
+  it('should throw an exception when drawCircle() is called', () => {
+    let sut = new AbstractDecorator({}, TEST_CONFIG);
+    expect(sut.drawCircle).to.throw(Error, 'Error! AbstractDecorator#drawCircle must be overridden');
+  });
+
+  it('should throw an exception when setLinePoints() is called', () => {
+    let sut = new AbstractDecorator({}, TEST_CONFIG);
+    expect(sut.setLinePoints).to.throw(Error, 'Error! AbstractDecorator#setLinePoints must be overridden');
   });
 });
--- test/SolidGenerator.spec.js
+++ test/SolidGenerator.spec.js
@@ -5,46 +5,48 @@ require('chai').should();
 
 describe('BaseGenerator class', () => {
 
-    it('should set default config on construction', () => {
-        let sut = new SolidGenerator();
-        let expected = {
-            numCircles: 1,
-            minMaxRad: 200,
-            maxMaxRad: 200,
-            minRadFactor: 0.2,
-            lineWidth: 2,
-            bgColor: '#ffffff',
-            urlColor: '#000000',
-            drawsPerFrame: 6,
-            canvasWidth: 200,
-            canvasHeight: 200
-        };
-        sut.config.should.deep.equal(expected);
-    });
+  it('should set default config on construction', () => {
+    let sut = new SolidGenerator();
+    let expected = {
+      numCircles: 1,
+      minMaxRad: 200,
+      maxMaxRad: 200,
+      minRadFactor: 0.2,
+      lineWidth: 2,
+      bgColor: '#ffffff',
+      urlColor: '#000000',
+      drawsPerFrame: 6,
+      canvasWidth: 200,
+      canvasHeight: 200,
+      iterations: 8
+    };
+    sut.config.should.deep.equal(expected);
+  });
 
-    it('should override config values passed to constructor', () => {
-        let newConfig = {
-            numCircles: 5,
-            minMaxRad: 1000
-        };
-        let expected = {
-            numCircles: 5,
-            minMaxRad: 1000,
-            maxMaxRad: 200,
-            minRadFactor: 0.2,
-            lineWidth: 2,
-            bgColor: '#ffffff',
-            urlColor: '#000000',
-            drawsPerFrame: 6,
-            canvasWidth: 200,
-            canvasHeight: 200
-        };
-        let sut = new SolidGenerator(newConfig);
-        sut.config.should.deep.equal(expected);
-    });
+  it('should override config values passed to constructor', () => {
+    let newConfig = {
+      numCircles: 5,
+      minMaxRad: 1000
+    };
+    let expected = {
+      numCircles: 5,
+      minMaxRad: 1000,
+      maxMaxRad: 200,
+      minRadFactor: 0.2,
+      lineWidth: 2,
+      bgColor: '#ffffff',
+      urlColor: '#000000',
+      drawsPerFrame: 6,
+      canvasWidth: 200,
+      canvasHeight: 200,
+      iterations: 8
+    };
+    let sut = new SolidGenerator(newConfig);
+    sut.config.should.deep.equal(expected);
+  });
 
-    it('should create a canvas object', () => {
-        let sut = new SolidGenerator();
-        expect(sut.createCanvas()).to.be.ok;
-    });
+  it('should create a canvas object', () => {
+    let sut = new SolidGenerator();
+    expect(sut.createCanvas()).to.be.ok;
+  });
 });
